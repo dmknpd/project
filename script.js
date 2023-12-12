f2063c4b30ecc56f0233ae450e002aedbcacdf66
Dmitriy Sochenko <soupuabt@gmail.com>
2023-12-12 11:58:11 +0200

    first practice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const pr = [1, 2, 3];
+let numbersOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+let personalMobieDB = {
+  count: numbersOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastMovie = prompt("Один из последних просмотренных фильмов?");
+
+personalMobieDB.movies[lastMovie] = prompt("На сколько оцените его");
+
+lastMovie = prompt("Один из последних просмотренных фильмов?");
+
+personalMobieDB.movies[lastMovie] = prompt("На сколько оцените его");
+
+console.log(personalMobieDB.movies);
